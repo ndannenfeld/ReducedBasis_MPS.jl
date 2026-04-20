9583691c96d9b06d96fd1e7f6aa59e20d80436c2
Nicolas Dannenfeld <ndannenf@smail.uni-koeln.de>
2026-04-20 09:07:50 +0200

    Update mps.jl

--- src/mps.jl
+++ src/mps.jl
@@ -181,7 +181,7 @@ function solve(H::AffineDecomposition, μ, Ψ₀::Vector{MPS}, dm::DMRG)
         end
     end
 
-    (; values, vectors, variances, iterations, maxtruncerr)
+    (; values, vectors[1], variances, iterations, maxtruncerr)
 end
 
 solve(H::AffineDecomposition, μ, Ψ₀::MPS, dm::DMRG) = solve(H, μ, [Ψ₀], dm)
