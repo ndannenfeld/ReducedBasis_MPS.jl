57d19e1dfcff6fb056c023c81d96ad1e4fbda5f3
Nicolas Dannenfeld <ndannenf@smail.uni-koeln.de>
2026-04-21 11:08:57 +0200

    Refactor rbasis.jl: Update truncation logic and vectors_new
    Comment out cutoff check and related logic, set vectors_new to identity matrix.

--- src/rbasis.jl
+++ src/rbasis.jl
@@ -198,7 +198,7 @@ function extend(basis::RBasis, new_snapshot::AbstractVector, μ, ed::EigenDecomp
     Λ = abs.(Λ)  # Remove zero elements with negative sign
     λ_error_trunc = 0.0
     keep = 1
-    if !iszero(ed.cutoff)
+    #=if !iszero(ed.cutoff)
         λ²_psums      = reverse(cumsum(Λ.^2))  # Reverse to put largest eigenvector sum first
         λ²_errors     = @. sqrt(λ²_psums / λ²_psums[1])
         idx_trunc     = findlast(err -> err > ed.cutoff, λ²_errors)
@@ -214,11 +214,11 @@ function extend(basis::RBasis, new_snapshot::AbstractVector, μ, ed::EigenDecomp
             overlaps = overlaps[1:idx_trunc, 1:idx_trunc]
             λ_error_trunc = λ²_errors[idx_trunc + 1]
         end
-    end
+    end=#
     snapshots   = append!(copy(basis.snapshots), new_snapshot[1:keep])  # TODO: use ordering of Λ
     parameters  = append!(copy(basis.parameters), fill(μ, keep))
-    vectors_new = U * Diagonal(1 ./ sqrt.(Λ))
-
+    #vectors_new = U * Diagonal(1 ./ sqrt.(Λ))
+vectors_new = I
     (; basis=RBasis(snapshots, parameters, vectors_new,
                     overlaps, vectors_new' * overlaps * vectors_new),
      keep, Λ, λ_error_trunc)
