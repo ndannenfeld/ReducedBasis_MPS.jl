34aeb27585f60122a2a7224572821a6603647223
Nicolas Dannenfeld <ndannenf@smail.uni-koeln.de>
2026-04-22 09:07:00 +0200

    Comment out conditional assignment for n==3
    Comment out code for n==3 case in greedy.jl

--- src/greedy.jl
+++ src/greedy.jl
@@ -185,8 +185,8 @@ function assemble(info::NamedTuple, H::AffineDecomposition, grid, greedy::Greedy
         μ_next = grid[idx_max]
         if n == 2
         μ_next = grid[1]
-        elseif n==3
-            μ_next=grid[Int(floor(length(grid)/2))]
+        #=elseif n==3
+            μ_next=grid[Int(floor(length(grid)/2))]=#
         else
         μ_next = grid[idx_max]
         end
